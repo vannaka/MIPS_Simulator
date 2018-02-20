76d8b654cb861dd53ab39a64ee50354273ece441
Luke Mammen <lmammen96@gmail.com>
2018-02-20 08:24:38 -0600

    Added pipeline code files

---
+++ src/pipeline.h
@@ -0,0 +1,15 @@
+#ifndef PIPELINE_H
+#define PIPELINE_H
+
+// function declarations
+void handle_pipeline(); /*IMPLEMENT THIS*/
+void WB();/*IMPLEMENT THIS*/
+void MEM();/*IMPLEMENT THIS*/
+void EX();/*IMPLEMENT THIS*/
+void ID();/*IMPLEMENT THIS*/
+void IF();/*IMPLEMENT THIS*/
+void show_pipeline();/*IMPLEMENT THIS*/
+void print_program(); /*IMPLEMENT THIS*/
+
+
+#endif
