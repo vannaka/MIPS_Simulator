8c4edd3fc3750aa8c00f9a1ba3b0962150c3e9bb
Luke Mammen <lmammen96@gmail.com>
2018-03-06 08:19:13 -0600

    Support larger input filenames

--- src/mu-mips.h
+++ src/mu-mips.h
@@ -21,7 +21,7 @@ int RUN_FLAG;	/* run flag*/
 uint32_t INSTRUCTION_COUNT;
 uint32_t PROGRAM_SIZE; /*in words*/
 
-char prog_file[32];
+char prog_file[64];
 
 
 /***************************************************************/
